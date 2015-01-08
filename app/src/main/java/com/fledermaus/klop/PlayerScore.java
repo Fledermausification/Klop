c762ff9b588ddeb7c86d21eeea5a33f8f40820b7
James <JMcCrean@Gmail.com>
2015-01-08 22:45:46 +1300

    Added some PlayerScore constants

--- app/src/main/java/com/fledermaus/klop/PlayerScore.java
+++ app/src/main/java/com/fledermaus/klop/PlayerScore.java
@@ -10,6 +10,11 @@ import java.util.ArrayList;
  * Created by James on 1/8/2015.
  */
 public class PlayerScore {
+    public static final int INITIAL_NUMBER_OF_SCORES = 10;
+    public static final int WINNING_SCORE            = 50;
+    public static final int MAX_MISS_COUNT           = 3;
+    public static final int MISS_PENALTY             = -10;
+
     private String               name;
     private TextView             nameLabel;
     private ArrayList <EditText> scores;
